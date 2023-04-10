efb3deb0765b16e62919c6bc9a1204309951a027
Finn Stokes <finn.stokes@gmail.com>
2023-04-10 23:36:01 +0200

    Made finite state machine more generic
    This allows it to be used for different state machines on the server and client

--- src/state.ts
+++ src/state.ts
@@ -1,5 +1,114 @@
 import { v4 as uuidv4 } from "uuid";
 
+type StateEvent<Events, E extends keyof Events = keyof Events> = Events[E] & {
+  type: E;
+};
+
+type Effects<Events, State> = {
+  ImmediateEffect: {
+    type: "immediate";
+    event: StateEvent<Events>;
+  };
+
+  DelayedEffect: {
+    type: "delayed";
+    afterMillis: number;
+    event: StateEvent<Events>;
+  };
+
+  PredicateEffect: {
+    type: "predicate";
+    timeout: number;
+    predicate: (state: State) => boolean;
+    event: StateEvent<Events>;
+  };
+};
+
+type Effect<Events, State, E extends keyof Effects<Events, State> = keyof Effects<Events, State>> = Effects<Events, State>[E];
+
+type EventHandler<Events, State, K extends keyof Events> = (
+  state: State,
+  event: StateEvent<Events, K>
+) => { state: State; effect?: Effect<Events, State> };
+
+type EventHandlers<Events, State> = {
+  [k in keyof Events]: EventHandler<Events, State, k>;
+};
+
+type EventListener = <Events, State>(state: State, event: StateEvent<Events>) => void;
+
+export const initialise = <Events, State>(initial: State, eventHandlers: EventHandlers<Events, State>): {
+  addListener: (listener: EventListener) => void,
+  dispatch: <E extends keyof Events>(event: StateEvent<Events, E>) => State,
+} => {
+  let currentState: State = initial;
+
+  const listeners: EventListener[] = [];
+
+  const addListener = (listener: EventListener) => {
+    listeners.push(listener);
+  };
+
+  let pendingPredicates: {
+    id: string;
+    timeout: NodeJS.Timeout;
+    predicate: (state: State) => boolean;
+    event: StateEvent<Events>;
+  }[] = [];
+
+  const dispatch = <E extends keyof Events>(
+    event: StateEvent<Events, E>
+  ): State => {
+    const { state, effect } = (
+      eventHandlers[event.type] as EventHandler<Events, State, typeof event.type>
+    )(currentState, event);
+
+    currentState = state;
+
+    switch (effect?.type) {
+      case "immediate":
+        dispatch(effect.event);
+        break;
+      case "delayed":
+        setTimeout(() => dispatch(effect.event), effect.afterMillis);
+        break;
+      case "predicate":
+        const id = uuidv4();
+        const timeout = setTimeout(() => {
+          pendingPredicates = pendingPredicates.filter((p) => p.id !== id);
+          dispatch(effect.event);
+        }, effect.timeout);
+        pendingPredicates.push({
+          ...effect,
+          timeout,
+          id,
+        });
+        break;
+    }
+
+    // Collect the events that predicates would fire, and make sure to remove them from the pending array before actioning
+    // to avoid recursion.
+    const resultingEvents: StateEvent<Events>[] = [];
+    pendingPredicates = pendingPredicates.flatMap((pendingPredicate) => {
+      const satisfied = pendingPredicate.predicate(state);
+      if (satisfied) {
+        resultingEvents.push(pendingPredicate.event);
+        clearTimeout(pendingPredicate.timeout);
+        return [];
+      }
+      else {
+        return [pendingPredicate];
+      }
+    });
+
+    resultingEvents.forEach(dispatch);
+
+    return state;
+  };
+
+  return {addListener, dispatch};
+};
+
 type Events = {
   ShoppingPhaseStart: {
     type: "ShoppingPhaseStart";
@@ -14,42 +123,9 @@ type State = {
   phase: "init" | "shopping" | "combat";
 };
 
-type StateEvent<E extends keyof Events = keyof Events> = Events[E] & {
-  type: E;
-};
-
-type Effects = {
-  ImmediateEffect: {
-    type: "immediate";
-    event: StateEvent;
-  };
-
-  DelayedEffect: {
-    type: "delayed";
-    afterMillis: number;
-    event: StateEvent;
-  };
-
-  PredicateEffect: {
-    type: "predicate";
-    timeout: number;
-    predicate: (state: State) => boolean;
-    event: StateEvent;
-  };
-};
-
-type Effect<E extends keyof Effects = keyof Effects> = Effects[E];
-
-type EventHandler<E extends StateEvent> = (
-  state: State,
-  event: E
-) => { state: State; effect?: Effect };
-
-type EventHandlers = {
-  [k in keyof Events]: EventHandler<Events[k]>;
-};
-
-const eventHandlers: EventHandlers = {
+export const {addListener, dispatch} = initialise<Events, State>({
+  phase: "init",
+}, {
   ShoppingPhaseStart: () => ({
     state: { phase: "shopping" },
     effect: {
@@ -70,73 +146,4 @@ const eventHandlers: EventHandlers = {
       },
     },
   }),
-};
-
-let currentState: State = {
-  phase: "init",
-};
-
-type EventListener = (state: State, event: StateEvent) => void;
-
-const listeners: EventListener[] = [];
-
-export const addListener = (listener: EventListener) => {
-  listeners.push(listener);
-};
-
-let pendingPredicates: {
-  id: string;
-  timeout: NodeJS.Timeout;
-  predicate: (state: State) => boolean;
-  event: StateEvent;
-}[] = [];
-
-export const dispatch = <E extends keyof Events>(
-  event: StateEvent<E>
-): State => {
-  const { state, effect } = (
-    eventHandlers[event.type] as EventHandler<typeof event>
-  )(currentState, event);
-  
-  currentState = state;
-
-  switch (effect?.type) {
-    case "immediate":
-      dispatch(effect.event);
-      break;
-    case "delayed":
-      setTimeout(() => dispatch(effect.event), effect.afterMillis);
-      break;
-    case "predicate":
-      const id = uuidv4();
-      const timeout = setTimeout(() => {
-        pendingPredicates = pendingPredicates.filter((p) => p.id !== id);
-        dispatch(effect.event);
-      }, effect.timeout);
-      pendingPredicates.push({
-        ...effect,
-        timeout,
-        id,
-      });
-      break;
-  }
-
-  // Collect the events that predicates would fire, and make sure to remove them from the pending array before actioning
-  // to avoid recursion.
-  const resultingEvents: StateEvent[] = [];
-  pendingPredicates = pendingPredicates.flatMap((pendingPredicate) => {
-    const satisfied = pendingPredicate.predicate(state);
-    if (satisfied) {
-      resultingEvents.push(pendingPredicate.event);
-      clearTimeout(pendingPredicate.timeout);
-      return [];
-    }
-    else {
-      return [pendingPredicate];
-    }
-  });
-
-  resultingEvents.forEach(dispatch);
-
-  return state;
-};
+});
